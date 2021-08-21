b82633eda3a9b135330d3086b54f4c7ac1beec2e
xxBen <benpro100@gmail.com>
2021-08-21 00:15:39 -0500

    (RC 8) LAST PROBLEM DONE, PLUGIN LIKELY FINISHED

--- src/main/java/com/parkourcraft/parkour/gameplay/SelectionListener.java
+++ src/main/java/com/parkourcraft/parkour/gameplay/SelectionListener.java
@@ -45,39 +45,27 @@ public class SelectionListener {
             Player player = Bukkit.getPlayer(actor.getName());
 
             // only continue if not opped
-            if (player != null && !player.isOp()) {
+            if (player != null) {
+                // if opped and bypassing plots, return
+                if (player.isOp() && Parkour.getStatsManager().get(player).isBypassingPlots())
+                    return;
+
                 // try catch for incomplete regions
                 try {
                     // get their session, the region from that selection, and the min/max points
                     LocalSession session = WorldEdit.getInstance().getSessionManager().findByName(player.getName());
                     Region region = session.getSelection(event.getWorld());
-                    Vector maxPoint = region.getMaximumPoint();
-                    Vector minPoint = region.getMinimumPoint();
 
-                    PlotsManager plotsManager = Parkour.getPlotsManager();
-                    World world = Bukkit.getWorld(Parkour.getSettingsManager().player_submitted_world);
-
-                    // get plot from min and max locations
-                    Plot plotInMin = plotsManager.getPlotInLocation(new Location(world, minPoint.getX(), minPoint.getY(), minPoint.getZ()));
-                    Plot plotInMax = plotsManager.getPlotInLocation(new Location(world, maxPoint.getX(), maxPoint.getY(), maxPoint.getZ()));
-
-                /*
-                    this logic is a bit complicated in a couple ways:
-
-                    1) we have to make sure they did not set a pos in a buffer width between plots (would return null)
-                    2) if it is not a road, then it is fine to use the plot to see if they are trusted or own it,
-                       if either are false, then it would cancel
-                    3) finally, we have to see if they are not the same plots as the player could be trusted in the plot
-                       of one of their pos and another pos in their own plot, therefore the selection goes over the road
-                       and across multiple plots
-                 */
-                    boolean cantInMin = plotInMin == null || !plotInMin.canBuild(player.getName());
-                    boolean cantInMax = plotInMax == null || !plotInMax.canBuild(player.getName());
-
-                    if (cantInMin || cantInMax || ((plotInMin != null && plotInMax != null) && !plotInMin.getOwnerName().equalsIgnoreCase(plotInMax.getOwnerName()))) {
+                    if (checkSelection(region.getMinimumPoint(), region.getMaximumPoint(), player)) {
                         // use FAWE api to cancel and send message
                         event.setCancelled(true);
-                        player.sendMessage(Utils.translate("&cThis WorldEdit selection is out of where you can build"));
+
+                        // send bypass info if opped
+                        String messageToSend = "&cThis WorldEdit selection is out of where you can build";
+                        if (player.isOp())
+                            messageToSend += " &7You can bypass this with &c/plot bypass";
+
+                        player.sendMessage(Utils.translate(messageToSend));
                     }
                 } catch (IncompleteRegionException e) {
                     // dont print stack track so we dont spam console with simple error
@@ -87,56 +75,86 @@ public class SelectionListener {
     }
 
     @Subscribe (priority = EventHandler.Priority.VERY_EARLY)
-    public void onPaste(PasteEvent event) throws EmptyClipboardException {
+    public void onPaste(PasteEvent event) {
 
         com.sk89q.worldedit.entity.Player wePlayer = event.getPlayer();
         Player player = Bukkit.getPlayer(wePlayer.getName());
 
-        if (player != null && !player.isOp() &&
+        if (player != null &&
             player.getWorld().getName().equalsIgnoreCase(Parkour.getSettingsManager().player_submitted_world)) {
 
-            LocalSession session = WorldEdit.getInstance().getSessionManager().findByName(player.getName());
-            ClipboardHolder holder = session.getClipboard();
+            // if opped and bypassing plots, return
+            if (player.isOp() && Parkour.getStatsManager().get(player).isBypassingPlots())
+                return;
 
-            /*AffineTransform transform = new AffineTransform();
-            transform = transform.rotateY(-(yRotate != null ? yRotate : 0));
-            transform = transform.rotateX(-(xRotate != null ? xRotate : 0));
-            transform = transform.rotateZ(-(zRotate != null ? zRotate : 0));
+            try {
+                // get session and clipboard holder of session
+                LocalSession session = WorldEdit.getInstance().getSessionManager().findByName(player.getName());
+                ClipboardHolder holder = session.getClipboard();
 
-            holder.setTransform(holder.getTransform().combine(transform));
-            Vector min = event.getClipboard().getMinimumPoint();
-            Vector diff = min.subtract(event.getClipboard().getOrigin());
-            Vector pastedMin = event.getPosition().add(diff);
-            Vector pastedMax = pastedMin.add(event.getClipboard().getMaximumPoint().subtract(event.getClipboard().getMinimumPoint()));*/
+                /*
+                    this is a complicated part because the location of
+                    the min/max of the new paste is not stored, so we have to do hacky math
 
-            // TODO: actually make this adjust for rotations, so far ive tried practically everything
-            // https://github.com/IntellectualSites/FastAsyncWorldEdit/blob/main/worldedit-core/src/main/java/com/sk89q/worldedit/command/ClipboardCommands.java#L529-L539
-            // code from here ^
+                    how it is done
+                    1) first we get the offset of the clipboard by subtracting the minimum point of the current clipboard
+                       by the origin
+                    2) we then apply that offset to the holder's transform (rotation, position etc) and add that
+                       to the position (gives us the new max)
+                    3) finally, we apply the max clipboard point subtracted by the min and then apply that to the transform,
+                       and add that to the max point, giving us the min
+                 */
+                Vector clipboardOffset = event.getClipboard().getMinimumPoint().subtract(event.getClipboard().getOrigin());
+                Vector max = event.getPosition().add(holder.getTransform().apply(clipboardOffset));
+                Vector min = max.add(holder.getTransform()
+                                    .apply(event.getClipboard().getMaximumPoint()
+                                    .subtract(event.getClipboard().getMinimumPoint())));
 
-            /*EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(event.getPlayer().getWorld(), -1, wePlayer);
-            Operation operation = holder.createPaste(editSession.getExtent(), editSession.getWorldData())
-                                 .to(holder.getClipboard().getOrigin())
-                                 .ignoreAirBlocks(false).build();
-            Operations.completeLegacy(operation);*/
+                if (checkSelection(min, max, player)) {
+                    event.setCancelled(true);
 
-            // OPTION 2
+                    // send bypass info if opped
+                    String messageToSend = "&cYour paste selection is out of where you can build";
+                    if (player.isOp())
+                        messageToSend += " &7You can bypass this with &c/plot bypass";
 
-            Vector clipboardOffset = event.getClipboard().getRegion().getMinimumPoint().subtract(event.getClipboard().getOrigin());
-            Vector realTo = event.getPosition().add(holder.getTransform().apply(clipboardOffset));
-            Vector max = realTo.add(holder
-                    .getTransform()
-                    .apply(event.getClipboard().getRegion().getMaximumPoint().subtract(event.getClipboard().getMinimumPoint())));
-
-            Vector min = max.subtract(event.getPlayer().getLocation().toVector());
-            Bukkit.broadcastMessage(min + " -> " + max);
-
-
-            /*RegionSelector selector = new CuboidRegionSelector(wePlayer.getWorld(), event.getPosition().toBlockPoint(), max.toBlockPoint());
-            session.setRegionSelector(wePlayer.getWorld(), selector);
-            selector.learnChanges();
-            selector.explainRegionAdjust(wePlayer, session);*/
-
-            //Bukkit.broadcastMessage(session.getClipboard().getClipboard().getMinimumPoint() + " -> " + session.getClipboard().getClipboard().getMaximumPoint());
+                    player.sendMessage(Utils.translate(messageToSend));
+                }
+            } catch (EmptyClipboardException e) {
+                // dont print stack track so we dont spam console with simple error
+            }
         }
+    }
+
+    // method to check if a selection will pass if they are in their plot or a trusted plot
+    private boolean checkSelection(Vector min, Vector max, Player player) {
+        boolean selectionPasses = false;
+
+        // get world
+        World plotWorld = Bukkit.getWorld(Parkour.getSettingsManager().player_submitted_world);
+
+        // get plots in min and max locations
+        Plot minPlot = Parkour.getPlotsManager().getPlotInLocation(new Location(plotWorld, min.getX(), min.getY(), min.getZ()));
+        Plot maxPlot = Parkour.getPlotsManager().getPlotInLocation(new Location(plotWorld, max.getX(), max.getY(), max.getZ()));
+
+        /*
+            this logic is a bit complicated in a couple ways:
+
+            1) we have to make sure they did not set a pos in a buffer width between plots (would return null)
+            2) if it is not a road, then it is fine to use the plot to see if they are trusted or own it,
+               if either are false, then it would cancel
+            3) finally, we have to see if they are not the same plots as the player could be trusted in the plot
+               of one of their pos and another pos in their own plot, therefore the selection goes over the road
+               and across multiple plots
+         */
+        boolean cantInMin = minPlot == null || !minPlot.canBuild(player.getName());
+        boolean cantInMax = maxPlot == null || !maxPlot.canBuild(player.getName());
+        boolean notSameOwner = ((minPlot != null && maxPlot != null) &&
+                !minPlot.getOwnerName().equalsIgnoreCase(maxPlot.getOwnerName()));
+
+        if (cantInMin || cantInMax || notSameOwner)
+            selectionPasses = true;
+
+        return selectionPasses;
     }
 }
