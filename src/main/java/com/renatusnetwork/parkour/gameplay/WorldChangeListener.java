f2d829ad5e148a9bf236c310629d2eaa79d0970c
xxBen <benpro100@gmail.com>
2021-08-28 09:42:15 -0500

    RC 11 - Final product before release, handle inv clearing from worlds

--- src/main/java/com/renatusnetwork/parkour/gameplay/WorldChangeListener.java
+++ src/main/java/com/renatusnetwork/parkour/gameplay/WorldChangeListener.java
@@ -3,10 +3,14 @@ package com.renatusnetwork.parkour.gameplay;
 import com.renatusnetwork.parkour.Parkour;
 import com.renatusnetwork.parkour.data.stats.PlayerStats;
 import com.renatusnetwork.parkour.data.stats.StatsManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
 
 public class WorldChangeListener implements Listener {
 
@@ -23,5 +27,16 @@ public class WorldChangeListener implements Listener {
         // if they are switching to not ascendance world and were in ascendance, remove them
         else if (statsManager.isInAscendance(playerStats))
             statsManager.leftAscendance(playerStats);
+
+        // if going to/from plot world, clear inv, armor and potions
+        if (player.getWorld().getName().equalsIgnoreCase(Parkour.getSettingsManager().player_submitted_world) ||
+            event.getFrom().getName().equalsIgnoreCase(Parkour.getSettingsManager().player_submitted_world)) {
+            player.getInventory().clear();
+            player.getInventory().setArmorContents(null);
+
+            // clear potion effects
+            for (PotionEffect potionEffect : player.getActivePotionEffects())
+                player.removePotionEffect(potionEffect.getType());
+        }
     }
 }
