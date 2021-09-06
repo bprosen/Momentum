e9a3a3df82a5ec4ff36c0b77891500e9e189acb1
xxBen <benpro100@gmail.com>
2021-09-06 17:56:36 -0500

    make it so you only need to be on ground to enable /prac

--- src/main/java/com/renatusnetwork/parkour/commands/PracticeCMD.java
+++ src/main/java/com/renatusnetwork/parkour/commands/PracticeCMD.java
@@ -24,35 +24,34 @@ public class PracticeCMD implements CommandExecutor {
             PlayerStats playerStats = Parkour.getStatsManager().get(player);
 
             if (playerStats.getLevel() != null) {
-                if (player.isOnGround()) {
-                    if (!playerStats.inRace()) {
-                        if (playerStats.getPlayerToSpectate() == null) {
-                            if (!playerStats.isEventParticipant()) {
+                if (!playerStats.inRace()) {
+                    if (playerStats.getPlayerToSpectate() == null) {
+                        if (!playerStats.isEventParticipant()) {
 
-                                // if it is a dropper level, disable /prac
-                                if (playerStats.inLevel() && playerStats.getLevel().isDropperLevel()) {
-                                    player.sendMessage(Utils.translate("&cPractice is disabled in &3&lDropper &clevels"));
-                                    return true;
-                                }
+                            // if it is a dropper level, disable /prac
+                            if (playerStats.inLevel() && playerStats.getLevel().isDropperLevel()) {
+                                player.sendMessage(Utils.translate("&cPractice is disabled in &3&lDropper &clevels"));
+                                return true;
+                            }
 
-                                if (playerStats.getPracticeLocation() != null) {
-                                    PracticeHandler.resetPlayer(player, true);
-                                } else {
-                                    playerStats.setPracticeMode(player.getLocation());
-                                    playerStats.disableLevelStartTime();
-                                    player.sendMessage(Utils.translate("&aYou have enabled practice mode and a temporary checkpoint has been set"));
-                                }
+                            // check practice location first, if not null, reset, then check if they are on ground, if they are, enable, if not, cancel
+                            if (playerStats.getPracticeLocation() != null) {
+                                PracticeHandler.resetPlayer(player, true);
+                            } else if (player.isOnGround()) {
+                                playerStats.setPracticeMode(player.getLocation());
+                                playerStats.disableLevelStartTime();
+                                player.sendMessage(Utils.translate("&aYou have enabled practice mode and a temporary checkpoint has been set"));
                             } else {
-                                player.sendMessage(Utils.translate("&cYou cannot do this while in an event"));
+                                player.sendMessage(Utils.translate("&cYou cannot enable practice mode while in the air"));
                             }
                         } else {
-                            player.sendMessage(Utils.translate("&cYou cannot enter practice mode while in /spectator"));
+                            player.sendMessage(Utils.translate("&cYou cannot do this while in an event"));
                         }
                     } else {
-                        player.sendMessage(Utils.translate("&cYou cannot do this while racing"));
+                        player.sendMessage(Utils.translate("&cYou cannot enter practice mode while in /spectator"));
                     }
                 } else {
-                    player.sendMessage(Utils.translate("&cYou cannot enable/disable practice mode while in the air"));
+                    player.sendMessage(Utils.translate("&cYou cannot do this while racing"));
                 }
             } else {
                 player.sendMessage(Utils.translate("&cYou cannot enter practice mode when not in a level"));
