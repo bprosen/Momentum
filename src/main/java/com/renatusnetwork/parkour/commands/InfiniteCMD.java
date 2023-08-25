3f11054bc865e999557e643bcdbb6b82a943a616
xxBen <benpro100@gmail.com>
2023-08-24 21:31:35 -0500

    format number

--- src/main/java/com/renatusnetwork/parkour/commands/InfiniteCMD.java
+++ src/main/java/com/renatusnetwork/parkour/commands/InfiniteCMD.java
@@ -152,11 +152,11 @@ public class InfiniteCMD implements CommandExecutor {
 
                     for (InfiniteReward reward : rewards)
                     {
-                        String msg = "&7" + position + " &5" + reward.getScoreNeeded() + " Score &7- &d" + reward.getDisplay();
+                        String msg = "&7" + position + " &5" + Utils.formatNumber(reward.getScoreNeeded()) + " Score &7- &d" + reward.getDisplay();
 
                         // send crossed out msg if their high score is more than the score needed
                         if (playerStats.getBestInfiniteScore() >= reward.getScoreNeeded())
-                            msg = "&7" + position + " &5&m" + reward.getScoreNeeded() + " Score&7 - &d" + reward.getDisplay();
+                            msg = "&7" + position + " &5&m" + Utils.formatNumber(reward.getScoreNeeded()) + " Score&7 - &d" + reward.getDisplay();
 
                         player.sendMessage(Utils.translate(msg));
                         position++;
