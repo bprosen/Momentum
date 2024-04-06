9cb04f1ea0b889918f443703a42b7dc6b358a33d
xxBen <benpro100@gmail.com>
2024-04-05 20:12:07 -0500

    fix auto save in previewing level bug

--- src/main/java/com/renatusnetwork/momentum/data/saves/SavesManager.java
+++ src/main/java/com/renatusnetwork/momentum/data/saves/SavesManager.java
@@ -50,6 +50,7 @@ public class SavesManager
                 playerStats.isAttemptingMastery() ||
                 playerStats.isInTutorial() ||
                 playerStats.isEventParticipant() ||
+                playerStats.isPreviewingLevel() ||
                 playerStats.inRace() ||
                 playerStats.isInInfinite() ||
                 !playerStats.inLevel() ||
